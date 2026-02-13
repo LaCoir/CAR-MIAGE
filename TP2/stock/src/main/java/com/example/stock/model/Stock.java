15cf8bbfc68afdab683ab986da6f1bdae2a9cb36
LaCoir <phuonglan.ng1011@gmail.com>
2026-02-13 01:08:28 +0100

    [TP2] Update Stock

--- TP2/stock/src/main/java/com/example/stock/model/Stock.java
+++ TP2/stock/src/main/java/com/example/stock/model/Stock.java
@@ -1,18 +1,30 @@
 package com.example.stock.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class Stock {
     @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private Float quantity;
 
+    public Stock(){}
     public Stock(String name, Float quantity) {
         this.name = name;
         this.quantity = quantity;
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
--- TP2/stock/src/main/java/com/example/stock/repository/StockRepository.java
+++ TP2/stock/src/main/java/com/example/stock/repository/StockRepository.java
@@ -4,6 +4,9 @@ import com.example.stock.model.Stock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface StockRepository extends CrudRepository<Stock, String> {
+public interface StockRepository extends CrudRepository<Stock, Long> {
+    Optional<Stock> findByName(String name);
 }
